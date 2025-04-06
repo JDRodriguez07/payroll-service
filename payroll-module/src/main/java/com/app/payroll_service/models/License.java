eb25b6074bcc4cf698d50af4684f218b77bbedae
JuanRodriguez <juandavid.03211@gmail.com>
2025-04-06 13:04:27 -0500

    refactor(models): remove redundant imports and add licenses field to LicenseType
    Remove unnecessary imports in License.java and add a one-to-many relationship between LicenseType and License with JsonIgnore annotation to prevent circular references

--- payroll-module/src/main/java/com/app/payroll_service/models/License.java
+++ payroll-module/src/main/java/com/app/payroll_service/models/License.java
@@ -1,11 +1,8 @@
 package com.app.payroll_service.models;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import jakarta.persistence.*;
 import lombok.Data;
 
--- payroll-module/src/main/java/com/app/payroll_service/models/LicenseType.java
+++ payroll-module/src/main/java/com/app/payroll_service/models/LicenseType.java
@@ -1,9 +1,12 @@
 package com.app.payroll_service.models;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
 import lombok.Data;
@@ -17,6 +20,10 @@ public class LicenseType {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "license_type_id", nullable = false)
     private Long licenseTypeId;
+
+    @OneToMany(mappedBy = "licenseType", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<License> licenses;
 
     @Column(name = "description", nullable = false)
     private String description;
