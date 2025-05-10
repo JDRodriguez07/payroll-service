c43f6d74300933a5f7fba3a6cb7bf93ec0e28dee
JuanRodriguez <juandavid.03211@gmail.com>
2025-05-10 13:52:18 -0500

    feat(swagger): add swagger configuration and update dependency
    Add SwaggerConfig class to enable API documentation and update springdoc-openapi-starter-webmvc-ui dependency to version 2.5.0 for enhanced functionality and compatibility

---
+++ payroll-module/src/main/java/com/app/payroll_service/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.app.payroll_service.config;
+
+// Importaciones necesarias para la configuración de Swagger
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Marca esta clase como una clase de configuración de Spring
+@Configuration
+public class SwaggerConfig {
+
+    // Define un bean que configura la documentación OpenAPI
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // Configura la información básica de la API
+                .info(new Info()
+                        .title("API de Gestión de Nómina") // Título de la API
+                        .version("1.0") // Versión de la API
+                        .description("API para la gestión de nómina desde el área de recursos humanos"));
+    }
+}
